c1d9deabbcb9a729c58dfaad74ad6ad9579ac66c
KrittametPlang <krittamet8251@gmail.com>
2025-12-04 14:23:14 +0700

    Fix import case for Navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Navbar } from './components/layout/navbar';
+import { Navbar } from './components/layout/Navbar';
 import { Footer } from './components/layout/Footer';
 import { AppRoutes } from './routes/AppRoutes';
 
